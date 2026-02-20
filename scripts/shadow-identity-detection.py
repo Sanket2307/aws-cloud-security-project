4d6da0500fa09f642d5ae32d9a48dc62b55f96c0
Sanket Bankar <sanketbankar4444@gmail.com>
2026-02-20 23:51:03 +0530

    Update shadow-identity-detection.py

--- scripts/shadow-identity-detection.py
+++ scripts/shadow-identity-detection.py
@@ -1,1 +1,12 @@
+import boto3
 
+iam = boto3.client("iam")
+
+users = iam.list_users()["Users"]
+
+print("\nShadow Identity Report\n------------------------")
+
+for user in users:
+    name = user["UserName"]
+    last_used = iam.get_user(UserName=name)
+    print(f"User: {name} → Last Used: {last_used}")
