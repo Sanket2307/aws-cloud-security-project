32b8a67cadf7029349825556d05035456778affb
Sanket Bankar <sanketbankar4444@gmail.com>
2026-02-20 23:50:22 +0530

    Update ec2-hardening.sh

--- scripts/ec2-hardening.sh
+++ scripts/ec2-hardening.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+sudo yum update -y
+sudo yum install -y fail2ban git amazon-cloudwatch-agent
+
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
+
+echo "System hardened and CloudWatch ready."
